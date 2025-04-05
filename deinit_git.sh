49db03f0abce3b7b1f1622642207053c9a07485b
oliverkirsch <oliver.kirsch@tn.mischok.academy>
2025-04-05 21:21:19 +0200

    responsiveness

--- deinit_git.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Setze das Root-Verzeichnis, in dem du starten möchtest
-root_dir="/home/oliverkirsch/hyperskill_projects_extract"
-
-# Durchlaufe alle Unterverzeichnisse und entferne das .git-Verzeichnis
-find "$root_dir" -type d -name ".git" -exec rm -rf {} \;
-
